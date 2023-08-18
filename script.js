ced2541154849c950550bacad1490584e5920997
Dallas Peterson <dallasrollinpeterson@gmail.com>
2023-08-18 02:04:15 -0700

    Implemented task remocal

--- script.js
+++ script.js
@@ -16,3 +16,10 @@ function addTask() {
 // Attach addTask function to the button click event
 const addButton = document.getElementById("addButton");
 addButton.addEventListener("click", addTask);
+
+//Add event listener to the entire task list for button slicks
+taskList.addEventListener("click", function(event){
+    if (event.taraget.classList.contains("remove-button")) {
+        event.target.parentNode.remove(); // Remove the parent <li> element
+    }
+});
